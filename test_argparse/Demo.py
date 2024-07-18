af2cc82b8152cad21a0766f37d30fa08d3370df7
TianfengChen <tfchan1999@gmail.com>
2024-07-18 11:08:42 -0400

    argparse example 1

---
+++ test_argparse/Demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+
+#argparse is a module that allows you to parse command line arguments
+
+parser = argparse.ArgumentParser(description='This is a simple program to add two numbers')
+parser.add_argument('--num1', help='The first number to add')
+parser.add_argument('--num2', action='store', help='The second number to add')
+
+args = parser.parse_args()
+if args.num1 is None or args.num2 is None:
+    print("You need to pass two numbers")
+else:
+    print(int(args.num1) + int(args.num2))
+#python3 Demo.py --num1 1 --num2 4
