da214a4c2326b13b3ef89f8a406bbd6bed82484e
TianfengChen <tfchan1999@gmail.com>
2024-07-26 17:01:46 -0400

    string processing

--- 4_StandardLibs/test_argparse/Demo.py
+++ 4_StandardLibs/test_argparse/Demo.py
@@ -6,9 +6,9 @@ import argparse
 #argparse is a module that allows you to parse command line arguments
 
 parser = argparse.ArgumentParser(description='This is a simple program to add two numbers')
-parser.add_argument('--num1', help='The first number to add')
+parser.add_argument('--n1','--num1', help='The first number to add')
 parser.add_argument('--num2', action='store', help='The second number to add')
-
+#default, 
 args = parser.parse_args()
 if args.num1 is None or args.num2 is None:
     print("You need to pass two numbers")
